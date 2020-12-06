f6671fb775aa0e6803823ccda6b7ba5967a6bfc3
Tarik-Amri <amri.tarik0@gmail.com>
2020-12-06 23:27:36 +0100

    added login

--- sc-react/src/App.js
+++ sc-react/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import Login from "./login";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    logged: false,
+    username: "",
+  };
+  render() {
+    const logstate = (username) => {
+      this.setState({ logged: true, username: username });
+    };
+    return (
+      <div className="App">
+        <AppBar style={{ backgroundColor: "#052640" }} position="sticky">
+          <Toolbar>
+            <Grid container>
+              <Grid item md={4}></Grid>
+              <Grid item xs={12} md={4}>
+                <img
+                  src="banner.png"
+                  alt="Second chances"
+                  height="80vh"
+                  style={{ padding: "10px" }}
+                ></img>
+              </Grid>
+            </Grid>
+          </Toolbar>
+          <Paper
+            style={{
+              backgroundColor: "#354e6b",
+              padding: "10px",
+            }}
+          >
+            <Typography variant="h5" style={{ color: "white" }}>
+              <b>
+                {this.state.logged ? (
+                  <span>Welcome {this.state.username}</span>
+                ) : (
+                  <span>Please log in, CMDR </span>
+                )}
+              </b>
+            </Typography>
+          </Paper>
+        </AppBar>
+        {this.state.logged ? (
+          <span />
+        ) : (
+          <Login logged={(username) => logstate(username)} />
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
